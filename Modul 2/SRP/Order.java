e6005e44f3464497a93413bcf280b10418ec5db0
tgarputustr <tegarputus@gmail.com>
2024-10-28 08:05:20 +0700

    Tambah Order

---
+++ Modul 2/SRP/Order.java
@@ -0,0 +1,28 @@
+package SRP;
+
+class Order { 
+    void calculateTotalSum() { 
+        /*...*/ 
+    } 
+    void getItems() { 
+        /*...*/ 
+    } 
+    void getItemCount() { 
+        /*...*/ 
+    } 
+    void addItem(Item item) { 
+        /*...*/ 
+    } 
+    void deleteItem(Item item) { 
+        /*...*/ 
+    } 
+    void printOrder() { 
+        /*...*/ 
+    } 
+    void showOrder() { 
+        /*...*/ 
+    } 
+ 
+    void getDailyHistory() {} 
+    void getMonthlyHistory() {} 
+}
