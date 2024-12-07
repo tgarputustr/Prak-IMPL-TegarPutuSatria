62e102c7a8a4c4f0f2e45f7033499a2fa1923722
tgarputustr <tegarputus@gmail.com>
2024-12-07 23:10:38 +0700

    Update DtaRcrd102

---
+++ Modul 5/CleanCode/DtaRcrd102.java
@@ -0,0 +1,9 @@
+package CleanCode;
+import java.sql.Date;
+
+public class DtaRcrd102 {
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+    /*...*/
+}
