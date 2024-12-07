29dd441eece0a902682baf0083e989fc30ec2127
tgarputustr <tegarputus@gmail.com>
2024-12-07 23:09:45 +0700

    Deleted CleanCode

--- Modul 5/Clean Code/Customer.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Date;
-
-public class Customer {
-    private Date generationTimestamp;
-    private Date modificationTimestamp;
-    private final String recordId = "102";
-    /*... */
-}
--- Modul 5/Clean Code/DtaRcrd102.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Date;
-
-public class DtaRcrd102 {
-    private Date genymdhms;
-    private Date modymdhms;
-    private final String pszqint = "102";
-    /*...*/
-}
