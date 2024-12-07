eff6f7fc190a18a80968fe74367802ae4c483a97
tgarputustr <tegarputus@gmail.com>
2024-12-07 23:30:47 +0700

    Add Journal Comment

---
+++ Modul 5/CleanCode/JournalComments.java
@@ -0,0 +1,17 @@
+package CleanCode;
+    /* 
+    * Changes (from 11-Oct-2001)
+    * --------------------------
+    * 11-Oct-2001 : Re-organised the class and moved it to new
+    * package com.jrefinery.date (DG)
+    * 05-Nov-2001 : Added a getDescription() method, and
+    * eliminated NotebaleDate class (DG)
+    * 12-Nov-2001 : IBD requires setDescription () method, now
+    * that NotableDate class is gone (DG); Changed
+    * getPreviousDayOfWeek(),
+    * getFollowingDayOfWeek() and
+    * getNearestDayOfWeek() to correct bugs (DG);
+    * 05-Dec-2001 : Fixed bug in SpreadsheetDate class (DG);
+    * 29-May-2002 : Moved the month constants into a separate
+    * interface (MonthConstants) (DG);
+    */
