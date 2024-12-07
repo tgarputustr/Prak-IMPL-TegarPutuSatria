c9a8395e319b4fbf13bc495191b58490def96c94
tgarputustr <tegarputus@gmail.com>
2024-12-07 22:34:14 +0700

    Add FinallyCatchBlock.java

---
+++ Modul 5/Exceptions/FInallyCatchBlock.java
@@ -0,0 +1,16 @@
+package Exceptions;
+public class FInallyCatchBlock {
+    public static void main(String[] args) {
+        try {
+            //protected code
+        } catch (ArithmeticException e) {
+            //catch block
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //catch block
+        } catch (Exception e){
+            //catch block
+        } finally {
+            //the finally block always execute
+        }
+    }
+}
