e1fb26547deda781c81166710d2a9d4906974528
tgarputustr <tegarputus@gmail.com>
2024-12-07 22:37:12 +0700

    Add Customer.java

---
+++ Modul 5/Clean Code/Customer.java
@@ -0,0 +1,8 @@
+import java.sql.Date;
+
+public class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+    /*... */
+}
