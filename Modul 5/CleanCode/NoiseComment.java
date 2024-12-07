abb9b18162f41e15c81ea350d8c9b53ea3b33be2
tgarputustr <tegarputus@gmail.com>
2024-12-07 23:31:01 +0700

    Add Noise Comment

---
+++ Modul 5/CleanCode/NoiseComment.java
@@ -0,0 +1,17 @@
+package CleanCode;
+public class NoiseComment {
+    /*
+     * Default constructor.
+     */
+    protected NoiseComment() {}
+    /** The day of the month */
+    private int dayOfMonth;
+    /**
+     * Returns the day of the month.
+     * @return the day of the month.
+     */
+    public int getDayofMonth() {
+        return dayOfMonth;
+    }
+    
+}
