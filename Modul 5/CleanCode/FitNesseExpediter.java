dfba96192843714d1d5e382dd4bef5b0d31b0ced
tgarputustr <tegarputus@gmail.com>
2024-12-08 06:53:34 +0700

    Add Bad Formatting

---
+++ Modul 5/CleanCode/FitNesseExpediter.java
@@ -0,0 +1,19 @@
+package CleanCode;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class FitNesseExpediter implements ResponseSender 
+{
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private Request request;
+    private Responder response;
+    private FitnesseContext context;
+    private long requestParsingTimeLimit;
+    private long requestProgress;
+    private long requestParsingDeadline;
+    private boolean hasError;
+}
