5660ace5e0733a7536c9fd0485c082697edd933d
tgarputustr <tegarputus@gmail.com>
2024-12-07 22:33:20 +0700

    Delete Exception

--- Modul 5/SingleCatchBlock.java
+++
@@ -1,9 +0,0 @@
-public class SingleCatchBlock {
-    public static void main(String[] args) {
-        try {
-            //protected code
-        } catch (Exception e) {
-            //catch block
-        }
-    }
-}
