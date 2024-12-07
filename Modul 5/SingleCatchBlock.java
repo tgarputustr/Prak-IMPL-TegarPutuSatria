d38105a923e4161cf638fac748bd955667d51703
tgarputustr <tegarputus@gmail.com>
2024-12-07 22:00:52 +0700

    Add SingleCatchBlock.java

---
+++ Modul 5/SingleCatchBlock.java
@@ -0,0 +1,9 @@
+public class SingleCatchBlock {
+    public static void main(String[] args) {
+        try {
+            //protected code
+        } catch (Exception e) {
+            //catch block
+        }
+    }
+}
