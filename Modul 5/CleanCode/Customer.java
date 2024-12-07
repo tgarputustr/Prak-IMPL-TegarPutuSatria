e21dcc839a54ee897bbb5376ef30131446588cc7
tgarputustr <tegarputus@gmail.com>
2024-12-07 23:10:51 +0700

    Update Customer.java

---
+++ Modul 5/CleanCode/Customer.java
@@ -0,0 +1,9 @@
+package CleanCode;
+import java.sql.Date;
+
+public class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+    /*... */
+}
