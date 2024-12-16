89f0258d3edc4bfd72b93584a5b40cba34d3412b
tgarputustr <tegarputus@gmail.com>
2024-12-16 08:53:10 +0700

    Add Simpan Pesan

---
+++ Modul 7/simpanPesan.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$idPengirim = $_POST['id'];
+$idPenerima = $_POST['Penerima'];
+$judul = $_POST['judul'];
+$TextPesan = $_POST['TextPesan'];
+$date = date('Y-m-d H:i:s');
+
+$conn = mysqli_connect("localhost", "root", "", "dbimpal");
+
+// Periksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO pesan (idPengirim, idPenerima, judul, TextPesan, date) VALUES ('$idPengirim', '$idPenerima', '$judul', '$TextPesan', '$date')";
+if (mysqli_query($conn, $sql)) {
+    echo "Pesan berhasil ditambahkan";
+} else {
+    echo "Pesan gagal ditambahkan: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
