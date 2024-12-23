333ddf7e7952a2c10153ac29ddf80deb0218c69c
tgarputustr <tegarputus@gmail.com>
2024-12-23 08:36:18 +0700

    Add Window

---
+++ Modul 8/Tugas/Window.java
@@ -0,0 +1,7 @@
+abstract class Window {
+    abstract void open();
+    abstract void close();
+    abstract void move();
+    abstract void display();
+    abstract void handleEvent(Event event);
+}
