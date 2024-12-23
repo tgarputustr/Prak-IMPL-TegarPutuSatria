1887fe8b14a11e841adc97fac7b8ce355257f475
tgarputustr <tegarputus@gmail.com>
2024-12-23 08:36:44 +0700

    Add Drawing Context

---
+++ Modul 8/Tugas/DrawingContext.java
@@ -0,0 +1,19 @@
+class DrawingContext {
+    public void setPoint() {
+        System.out.println("Setting Point");
+    }
+
+    public void clearScreen() {
+        System.out.println("Clearing Screen");
+    }
+
+    public int getVerticalSize() {
+        System.out.println("Getting Vertical Size");
+        return 0;
+    }
+
+    public int getHorizontalSize() {
+        System.out.println("Getting Horizontal Size");
+        return 0;
+    }
+}
