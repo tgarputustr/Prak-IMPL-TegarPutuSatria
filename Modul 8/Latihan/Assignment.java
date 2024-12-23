eca79433ab2286098e1916d342f06a820c38b81a
tgarputustr <tegarputus@gmail.com>
2024-12-23 07:34:21 +0700

    Add Assignment

---
+++ Modul 8/Latihan/Assignment.java
@@ -0,0 +1,13 @@
+public class Assignment {
+    private double mark=-1;
+    Assignment() { }
+    public double getMark() throws NotYetSetException {
+        if (mark==-1) {
+            throw new NotYetSetException("Mark is not yet");
+        }
+        return mark;
+    }
+    public void setMark(int m) {
+        mark = m;
+    }
+}
