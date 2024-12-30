686969b0d2e9ba2fa98d1689f03608eb666f835c
tgarputustr <tegarputus@gmail.com>
2024-12-30 13:09:18 +0700

    tambah kelas handled error

---
+++ Modul 9/ContohKasus/HandledErrors.java
@@ -0,0 +1,6 @@
+public class HandledErrors {
+    public static final String InvalidBothParameterMessage = "Both CustomerID and ProductNumber cannot be populated.";
+    public static final String CustomerEmptyMessage = "CustomerID and ProductNumber cannot both be empty.";
+    public static final String CustomerInvalidLengthMessage = "CustomerID exceeds the allowed length.";
+    public static final String ProductInvalidLengthMessage = "ProductNumber exceeds the allowed length.";
+}
