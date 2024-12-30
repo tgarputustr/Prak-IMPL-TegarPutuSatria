71441008e6351a1a8618a79624bb6cc329f643f1
tgarputustr <tegarputus@gmail.com>
2024-12-30 08:08:58 +0700

    add Person Sebelum Refactoring

---
+++ Modul 9/Refactoring Class/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public String name;
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Bob Smith";
+        System.out.println(person.name);
+    }
+}
